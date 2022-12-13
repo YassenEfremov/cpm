a5b17e8a1f7ef2250d2c38af151c1ed16e86c4db
Yassen Efremov <48584741+YassenEfremov@users.noreply.github.com>
2022-12-13 22:20:35 +0200

    Create basic CLI

---
+++ src/cli/parser.cpp
@@ -0,0 +1,57 @@
+#include "parser.h"
+
+#include "version.h"
+
+#include "argparse/argparse.hpp"
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+
+void parse_args(int argc, char *argv[]){
+
+    argparse::ArgumentParser parser("cpm", CPM_VERSION);
+
+    // Install command
+    argparse::ArgumentParser install_command("install");
+    install_command.add_description("Install the given package/s");
+    install_command.add_argument("packages")
+        .help("Packages to install")
+        .required()
+        .nargs(argparse::nargs_pattern::at_least_one);
+
+    // Remove command
+    argparse::ArgumentParser remove_command("remove");
+    remove_command.add_description("Remove the given package/s");
+    remove_command.add_argument("packages")
+        .help("Packages to remove")
+        .required()
+        .nargs(argparse::nargs_pattern::at_least_one);
+
+    // List command
+    argparse::ArgumentParser list_command("list");
+    list_command.add_description("List all installed packages");
+
+    // Update command
+    argparse::ArgumentParser update_command("update");
+    update_command.add_description("Update the given package/s");
+    update_command.add_argument("packages")
+        .help("Packages to update")
+        .required()
+        .nargs(argparse::nargs_pattern::at_least_one);
+
+    parser.add_subparser(install_command);
+    parser.add_subparser(remove_command);
+    parser.add_subparser(list_command);
+    parser.add_subparser(update_command);
+
+    try {
+        parser.parse_args(argc, argv);
+        if (argc < 2) throw std::runtime_error(parser.help().str());
+    } catch (const std::runtime_error &err) {
+        std::cerr << err.what() << std::endl;
+        // std::cerr << parser;
+        std::exit(EXIT_FAILURE);
+    }
+}
